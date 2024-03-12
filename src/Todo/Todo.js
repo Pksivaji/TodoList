4cb4e5a03f0c4185bca8aaea71b9a85c2b57dc1d
Sivaji Reddy <107327927+Pksivaji@users.noreply.github.com>
2024-03-12 23:08:06 +0530

    Add files via upload

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,1 +1,77 @@
 
+import './Todo.css'
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid' // you need to install this library using npm
+
+function Todo() {
+  let [todoInput, updateInput] = useState("");
+  let [todoList, updateTodos] = useState([]);
+
+  function addNewTodo() {
+    if (todoInput === "") {
+      alert("Add some task");
+    } else {
+      let newTodo = [
+        ...todoList,
+        {
+          id: uuidv4(), // this will generate a unique id for each todo
+          task: todoInput
+        }
+      ];
+      updateTodos(newTodo);
+      updateInput("");
+    }
+  }
+
+  function deleteTodo(id) {
+    let updatedTodos = todoList.filter((todo) => {
+      return todo.id !== id;
+    });
+    updateTodos(updatedTodos);
+  }
+
+  return (
+    <>
+      <div className='container mt-5 w-50 border border-success p-5 bg-dark'>
+       <center> <h3 className='text-success'>Todo List</h3></center>
+        <p className='text-primary'>What you want to do today</p>
+        <p>Write Down</p>
+        <form onSubmit={(e) => e.preventDefault()}>  
+          <div className='input-group'>
+            <input
+              type='text'
+              className='form-control'
+              value={todoInput}
+              onChange={(e) => {
+                let task = e.target.value;
+                updateInput(task);
+              }}
+            />
+            <button type="button" className='btn btn-primary' onClick={() => addNewTodo()}>
+              Add
+            </button>
+          </div>
+        </form>
+        <ul className='list-group mt-3'>
+          {todoList.map((todo) => {
+            return (
+              <li className='list-group-item d-flex li-gap' key={todo.id}>
+                <p>{todo.task}</p>
+                <button
+                  className='btn'
+                  onClick={() => {
+                    deleteTodo(todo.id);
+                  }}
+                >
+                  ❌
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Todo;
